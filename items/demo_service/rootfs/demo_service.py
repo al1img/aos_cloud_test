c7e6593031b27625b634a22d521f7cbd8e739c94
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2026-01-15 16:40:00 +0200

    items: add demo service item
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ items/demo_service/rootfs/demo_service.py
@@ -0,0 +1,13 @@
+import time
+
+import some_other_module
+
+
+def main():
+    while True:
+        print("Running demo service...")
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
